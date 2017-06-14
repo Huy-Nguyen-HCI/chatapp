8e3ec7fbe3127c2b2a3dc002a6f13abc72d908bf
Thang Le <thangminh.le208@gmail.com>
2017-06-13 22:29:17 -0400

    signup form now checks for duplicate usernames and emails

--- app/controllers/SignupController.scala
+++ app/controllers/SignupController.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import java.sql.SQLException
 import javax.inject.Inject
 
 import play.api.mvc._
@@ -12,7 +11,6 @@ import models.User
 import dao.UserDao
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
 import com.github.t3hnar.bcrypt._
 
 /**
@@ -46,19 +44,33 @@ class SignupController @Inject() (val messagesApi: MessagesApi, userDao: UserDao
         val password = userInfo.password
         val email = userInfo.email
 
-        for {
-          dbUsername <- userDao.findByUsername(username)
-          dbEmail <- userDao.findByEmail(email)
-          res <-
-            if (dbUsername.isEmpty && dbEmail.isEmpty) {
-              Redirect("/login")
-            } else {
-              Redirect("/")
-            }
-        } yield res
+        val dbUsername = userDao.findByUsername(username)
+        val dbEmail = userDao.findByEmail(email)
+
+        val isExist = for { v1 <- dbUsername; v2 <- dbEmail } yield (v1, v2)
+
+        isExist flatMap {
+          case (Some(_), _) =>
+            val errorForm = createErrorForm(userInfo, "username", "this username already exists")
+            Future.successful(Ok(views.html.signup(errorForm)))
+          case (_, Some(_)) =>
+            val errorForm = createErrorForm(userInfo, "email", "this email already exists")
+            Future.successful(Ok(views.html.signup(errorForm)))
+          case (None, None) =>
+            val newUser = User(username, password.bcrypt, email)
+            userDao.insert(newUser).map(_ => Redirect("/"))
+        }
+
       }
     )
   }
+
+  private def createErrorForm(info: UserSignupData, errorField: String, message: String) = {
+    val errorForm = userForm.withError(errorField, message)
+    val formFields = UserSignupData(info.username, "", "", info.email) // hide passwords
+    errorForm.fill(formFields)
+  }
+
 }
 
 // user sign up form
