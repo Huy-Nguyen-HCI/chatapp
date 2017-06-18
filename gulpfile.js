b20df2d9262c4a7d46c9e8f39c0034f89e897e41
Thang Le <thangminh.le208@gmail.com>
2017-06-17 22:24:06 -0400

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
-const gulp = require('gulp')
-const babel = require('gulp-babel')
+const gulp = require('gulp');
+const babel = require('gulp-babel');
 
-gulp.task('default', () =>
+// default task
+gulp.task('default', ['babel', 'watch'])
+
+// compile es6->es5 using babel
+gulp.task('babel', () =>
     gulp.src('es6scripts/**/*.js')
         .pipe(babel({
             presets: ['env']
         }))
         .pipe(gulp.dest('target/web/public/main/es6scripts'))
+);
+
+// watch for file saves
+gulp.task('watch', () =>
+    gulp.watch('es6scripts/**/*.js', ['babel'])
 );
