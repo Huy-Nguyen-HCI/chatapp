749990c507a00a8d21e28a1fc7e641eef36f1c96
Thang Le <thangminh.le208@gmail.com>
2017-08-05 17:21:20 -0700

    use Ctrl as syntax

--- public/javascripts/chat/chat.component.js
+++ public/javascripts/chat/chat.component.js
@@ -5,94 +5,98 @@ angular
   .module('chat')
   .component('chatWindow', {
     templateUrl: '/assets/javascripts/chat/chat.template.html',
-    controller: ['$scope', '$http', 'chatModel', 'Users',
-      function ($scope, $http, chatModel, Users) {
-
-        // websocket for sending chat messages
-        var ws = new WebSocket("ws://localhost:9000/chat/socket");
-
-        $scope.rooms = chatModel.getRooms();
-        $scope.currentRoom = $scope.rooms[0];
-
-        $scope.msgs = [];
-        $scope.inputText = "";
-
-        $scope.user = $('#connected-user').text();
-        $scope.csrfToken = $("#csrf-token").text();
-        $scope.filePickerClient = filestack.init('AqRfNWvWJTgcoBKncr9gCz');
-
-        angular.element(document).ready(function () {
-          // Save the math input box to scope variable
-          var MQElement = document.getElementById("mathquill");
-          var MQ = MathQuill.getInterface(2);
-          $scope.mathField = MQ.MathField(MQElement, {});
-          setupMathInput($scope.mathField);
-        });
-
-        /** change current room, restart EventSource connection */
-        $scope.setCurrentRoom = function (room) {
-          $scope.currentRoom = room;
-          $scope.msgs = [];
-        };
-
-        /** re-render the page whenever any model is updated **/
-        $scope.$watch(function(){
-          MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-          return true;
-        });
-
-        /** check if a message is nonempty and does not contain only space **/
-        function isInputValid(message) {
-          return message && message.replace(/\s/g, '').length > 0;
-        }
-
-        /** posting chat text */
-        $scope.submitMsg = function () {
-          var inputText = $scope.inputText;
-          if (!isInputValid(inputText)) return;
-
-          var sendData = {text: inputText};
-          $scope.msgs.push(sendData);
-          ws.send(JSON.stringify(sendData));
-
-          $scope.inputText = "";
-        };
-
-        ws.onmessage = function (msg) {
-          $scope.msgs.push(JSON.parse(msg.data));
-          MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-          $scope.$digest();
-        };
-
-        /** posting math formula */
-        $scope.submitMath = function() {
-          var mathInput = $scope.mathField.latex();
-
-          // if input is empty or only contains space, ignore
-          if (!isInputValid(mathInput)) return;
-
-          mathInput = "$$" + mathInput + "$$";
-          $scope.msgs.push(mathInput);
-          ws.send(mathInput);
-          $scope.mathField.latex("");
-        };
-
-        /** handle file upload */
-        $scope.showPicker = function() {
-          $scope.filePickerClient.pick({}).then(function (result) {
-            var files = result.filesUploaded;
-            for (var i = 0 ; i < files.length ; i++) {
-              $scope.displayFile(files[i].url, files[i].filename);
-            }
-            $scope.$digest();
-          });
-        };
-
-        $scope.displayFile = function(link, fileName) {
-          console.log("hello");
-          var sendData = {fileName: fileName, link: link};
-          $scope.msgs.push(sendData);
-          ws.send(JSON.stringify(sendData));
-        };
-      }]
+    controller: ChatWindowController,
+    controllerAs: 'chatCtrl'
   });
+
+ChatWindowController.$inject = ['$scope', 'chatModel'];
+
+function ChatWindowController($scope, chatModel) {
+  // websocket for sending chat messages
+  var vm = this;
+  var ws = new WebSocket("ws://localhost:9000/chat/socket");
+
+  vm.rooms = chatModel.getRooms();
+  vm.currentRoom = vm.rooms[0];
+
+  vm.msgs = [];
+  vm.inputText = "";
+
+  vm.user = $('#connected-user').text();
+  vm.csrfToken = $("#csrf-token").text();
+  vm.filePickerClient = filestack.init('AqRfNWvWJTgcoBKncr9gCz');
+
+  angular.element(document).ready(function () {
+    // Save the math input box to scope variable
+    var MQElement = document.getElementById("mathquill");
+    var MQ = MathQuill.getInterface(2);
+    vm.mathField = MQ.MathField(MQElement, {});
+    setupMathInput(vm.mathField);
+  });
+
+  /** change current room, restart EventSource connection */
+  vm.setCurrentRoom = function (room) {
+    vm.currentRoom = room;
+    vm.msgs = [];
+  };
+
+  /** re-render the page whenever any model is updated **/
+  $scope.$watch(function(){
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+    return true;
+  });
+
+  /** check if a message is nonempty and does not contain only space **/
+  function isInputValid(message) {
+    return message && message.replace(/\s/g, '').length > 0;
+  }
+
+  /** posting chat text */
+  vm.submitMsg = function () {
+    var inputText = vm.inputText;
+    if (!isInputValid(inputText)) return;
+
+    var sendData = {text: inputText};
+    vm.msgs.push(sendData);
+    ws.send(JSON.stringify(sendData));
+
+    vm.inputText = "";
+  };
+
+  ws.onmessage = function (msg) {
+    vm.msgs.push(JSON.parse(msg.data));
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    $scope.$digest();
+  };
+
+  /** posting math formula */
+  vm.submitMath = function() {
+    var mathInput = vm.mathField.latex();
+
+    // if input is empty or only contains space, ignore
+    if (!isInputValid(mathInput)) return;
+
+    mathInput = "$$" + mathInput + "$$";
+    vm.msgs.push(mathInput);
+    ws.send(mathInput);
+    vm.mathField.latex("");
+  };
+
+  /** handle file upload */
+  vm.showPicker = function() {
+    vm.filePickerClient.pick({}).then(function (result) {
+      var files = result.filesUploaded;
+      for (var i = 0 ; i < files.length ; i++) {
+        vm.displayFile(files[i].url, files[i].filename);
+      }
+      vm.$digest();
+    });
+  };
+
+  vm.displayFile = function(link, fileName) {
+    console.log("hello");
+    var sendData = {fileName: fileName, link: link};
+    vm.msgs.push(sendData);
+    ws.send(JSON.stringify(sendData));
+  };
+}
